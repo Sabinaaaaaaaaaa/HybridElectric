c63f74727189c5e68d0838965d9940328e9dddae
Sabinaaaaaaaaaa <sabinamh03@gmail.com>
2025-12-08 01:24:01 +0000

    added validation checks for power

--- src/mission.jl
+++ src/mission.jl
@@ -20,7 +20,7 @@ function runmission(FULLMISSION, Propulsion, Aircraft, W_PGD, W_battery, W_fuel_
     #total amount of capacity that the battery can provide!
     batterycapacity = total_battery_energycapacity(W_battery, Propulsion.specificenergy) 
 
-
+    batterydepleted=false
     for segment in FULLMISSION #this iterates across the FULLMISSION ARRAY, each segment is a MissionSegment
 
 
@@ -43,19 +43,14 @@ function runmission(FULLMISSION, Propulsion, Aircraft, W_PGD, W_battery, W_fuel_
             #calculate power required with the current weight estimate at this flight stage
             P_req=powerrequired(drag, segment.V, weight, segment.load*g, segment.dVdt, segment.ROC)/η 
 
-            #validation checks to see if battery and engine are able to deliver this!
-            P_max_available = Propulsion.P_max_motor + Propulsion.P_max_engine
-            if P_req>P_max_available
-                println("Power required exceeds maximum available power at time $(state.time) seconds. Required: $(P_req) W, Available: $(P_max_available) W")
-                return false, state.SOC
-            end
+            #validation checks to see if engine is able to deliver this!
 
             #power split
             P_EM_req, P_FB_req =powersplit(P_req, segment.ϕ)
 
-            if P_EM_req>Propulsion.P_max_motor 
-                println("Power EM required exceeds maximum available power at time $(state.time) seconds. Required: $(P_EM_req) W, Available: $(P_max_motor) W")
-                return false, state.SOC
+            if batterydepleted
+                P_FB_req += P_EM_req
+                P_EM_req = 0
             end
 
             if P_FB_req>Propulsion.P_max_engine 
@@ -71,10 +66,10 @@ function runmission(FULLMISSION, Propulsion, Aircraft, W_PGD, W_battery, W_fuel_
 
                 #check if battery is depleted
                 if SOC_new < Propulsion.SOC_min
-                    println("Battery depleted during mission segment! SOC dropped below 20%, need to increase battery size.")
-                    println("Battery depleted after $(state.time) seconds! Switching additional load to fuel-based propulsion")
+                    println("Battery depleted during mission segment after $(state.time) seconds! SOC dropped below 20%, need to increase battery size. Switching additional load to fuel-based propulsion.")
                     P_FB_req += P_EM_req  
                     P_EM_req = 0
+                    batterydepleted=true
                     # Don't update SOC below minimum
                 else
                     state.SOC = SOC_new
@@ -96,6 +91,9 @@ function runmission(FULLMISSION, Propulsion, Aircraft, W_PGD, W_battery, W_fuel_
             
             state.time+=dt
         end
+
+        println("Completed segment: $(segment.name), Time: $(state.time) seconds, SOC: $(round(state.SOC*100,digits=2))%, Remaining Fuel Mass: $(round(state.W_fuel,digits=2)) kg")
+
     end
     #misison is complete!
     return true, state.SOC
